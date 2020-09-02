17603e2a854c3d4c349039e6602c1c98fa404618
Aman Agrawal <aman.agrawal@ad.loginradius.net>
2020-09-02 15:07:22 +0530

    Created a simple todo application using react redux and react hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+const Todos = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector(state => state.todos);
+  const handleClick = id => dispatch({
+    type: 'DELETE_TODO',
+    payload: id
+  })
+  if (!todos || !todos.length) {
+    return <p>No Todos</p>
+  }
+
+  return (
+    <ul>
+      {todos.map(todo => <li>{todo.label} <span onClick={() => handleClick(todo.id)} class="close">×</span> </li>)}
+    </ul>
+  )
+}
+
+const TodoInput = () => {
+  const dispatch = useDispatch();
+  const [newTodo, setNewTodo] = useState();
+  const handleChange = event => setNewTodo(event.target.value);
+  const handleClick = () => dispatch({
+    type: 'ADD_TODO',
+    payload: {
+      label: newTodo,
+      id: Math.ceil(Math.random() * 100),
+    }
+  })
+
+  return (
+    <div className="header">
+      <input value={newTodo} onChange={handleChange} type="text" />
+      <span onClick={handleClick} className="addBtn">ADD TODO</span>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-container">
+        <h3>
+          To DO Application
+        </h3>
+        <TodoInput />
+        <Todos />
+      </div>
     </div>
   );
 }
